ec94df27bc3f8510f84919b88676391f15587d89
JackPatrickBlaiseBrennan <jackpbbrennan@gmail.com>
2022-01-04 20:04:10 +0000

    Init: Project Setup

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import Game from './Game';
+
+export default function App() {
+  return (
+    <Game/>
+  );
+}
+
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Game() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Simple Game</Text>
       <StatusBar style="auto" />
     </View>
   );
